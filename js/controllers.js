c5287ca64039642d86c31b4f38c3e972e2fdeadc
Jonathan Yee <raptrex@gmail.com>
2013-12-25 00:15:57 -0800

    added error message when user doesnt exist

--- js/controllers.js
+++ js/controllers.js
@@ -11,20 +11,25 @@ angular.module('GithubViewer.controllers', []).
             if ($scope.user.length > 2) {
                 $http.get('https://api.github.com/users/' + $scope.user + '/repos').
                     success(function(data) {
-                        $scope.repositories = data;
-                        var languages = [];
-                        $scope.languageFilter = [];
+                        if (data.length > 0) {
+                            $scope.repositories = data;
+                            $scope.error = false;
+                            var languages = [];
+                            $scope.languageFilter = [];
 
-                        for (var i = 0; i < data.length; i++) {
-                            // only add a language once
-                            if (data[i].language !== null &&
-                                languages.indexOf(data[i].language) === -1) {
-                                    languages.push(data[i].language);
-                                    $scope.languageFilter.push({
-                                        name : data[i].language,
-                                        selected : true
-                                    });
+                            for (var i = 0; i < data.length; i++) {
+                                // only add a language once
+                                if (data[i].language !== null &&
+                                    languages.indexOf(data[i].language) === -1) {
+                                        languages.push(data[i].language);
+                                        $scope.languageFilter.push({
+                                            name : data[i].language,
+                                            selected : true
+                                        });
+                                }
                             }
+                        } else {
+                            $scope.error = true;
                         }
                     });
             }
